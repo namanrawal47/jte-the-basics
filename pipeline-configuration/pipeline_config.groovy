b9910df378318a916ff18dac5afa9425d3c5e995
608217 <namanrawal@c02z70k8lvdt.users.bah.com>
2020-01-27 17:50:22 -0500

    added app env stuff

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -1,9 +1,20 @@
 libraries{
     sonarqube
+    ansible
 }
 stages{
     continuous_integration{
         build
         static_code_analysis
+    }
+}
+
+application_environments{
+    dev{
+        ip_addresses = [ "0.0.0.1", "0.0.0.2" ]
+    }
+    prod{
+        long_name = "Production"
+        ip_addresses = [ "0.0.1.1", "0.0.1.2". "0.0.1.3", "0.0.1.4" ]
     }
 }
