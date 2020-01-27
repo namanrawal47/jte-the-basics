f06bb802dfcf369d77158d2a9d9d1504d068da43
608217 <namanrawal@c02z70k8lvdt.users.bah.com>
2020-01-27 17:51:58 -0500

    fixing syntax

--- pipeline-configuration/pipeline_config.groovy
+++ pipeline-configuration/pipeline_config.groovy
@@ -15,6 +15,6 @@ application_environments{
     }
     prod{
         long_name = "Production"
-        ip_addresses = [ "0.0.1.1", "0.0.1.2". "0.0.1.3", "0.0.1.4" ]
+        ip_addresses = [ "0.0.1.1", "0.0.1.2", "0.0.1.3", "0.0.1.4" ]
     }
 }
