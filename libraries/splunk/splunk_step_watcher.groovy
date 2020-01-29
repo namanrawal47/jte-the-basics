1484399f2be8226ace08dc400653061c2c1fe242
608217 <namanrawal@c02z70k8lvdt.users.bah.com>
2020-01-29 12:10:45 -0500

    fixed

--- libraries/splunk/splunk_step_watcher.groovy
+++ libraries/splunk/splunk_step_watcher.groovy
@@ -3,4 +3,9 @@ void before(context){
 println "Splunk: running before the ${context.library} library's ${context.step} step"
 }
 
-@AfterStep({context.step in config.afterSteps})
+@AfterStep
+void after(context){
+println "Splunk: running after the ${context.library} library's ${context.step} step"
+}
+
+@AfterStep({ context.step in config.afterSteps })
